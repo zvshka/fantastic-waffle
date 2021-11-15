a3cabd284f6ccc602aa99c36f49360c8b99a76d7
zvshka <pushpurs@yandex.ru>
2021-11-15 09:43:45 +0300

    Next.js Version

--- components/Home/AdvantagesCard.js
+++ components/Home/AdvantagesCard.js
@@ -3,13 +3,9 @@ import Image from "next/image";
 const AdvantagesCard = ({image, text}) => {
     return (
         <>
-            <div className="flex flex-col items-center">
-                <div className="bg-primary text-white p-8 rounded-lg md:w-full mb-4">
-                    <div className="flex items-center justify-center">
-                        <Image src={image} width={160} height={160} alt={""}/>
-                    </div>
-                </div>
-                <p className="text-center text-lg">{text}</p>
+            <div className="bg-primary w-[16rem] flex items-center flex-col p-4 rounded-2xl">
+                <Image src={image} width={160} height={160} alt={""}/>
+                <p className="text-center text-lg text-white">{text}</p>
             </div>
         </>
     )
