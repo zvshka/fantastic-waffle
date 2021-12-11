6955e9f1009cbc775d6511bc87355b54f85f4f06
zvshka <pushpurs@yandex.ru>
2021-12-11 18:01:19 +0300

    Next.js Version

--- components/Dashboard/Navbar.js
+++ components/Dashboard/Navbar.js
@@ -1,18 +1,29 @@
 import {useState} from "react";
 
-export const Navbar = () => {
+export const Navbar = ({children}) => {
 
-    const [profileOpen, setOpen] = useState(false)
+    const [profileOpen, setProfileOpen] = useState(false)
+    const [submenuOpen, setSubmenuOpen] = useState(false)
+    const [mainOpen, setMainOpen] = useState(false)
+
+    const handleSubOpen = (e) => {
+        e.preventDefault()
+        setSubmenuOpen(!submenuOpen)
+    }
+    const handleMainOpen = (e) => {
+        e.preventDefault()
+        setMainOpen(!mainOpen)
+    }
     const handleOpen = (e) => {
         e.preventDefault()
-        setOpen(!profileOpen)
+        setProfileOpen(!profileOpen)
     }
 
     return <div className="flex flex-col flex-1 min-h-screen overflow-x-hidden overflow-y-auto">
         <header className="relative bg-white dark:bg-darker">
             <div className="flex items-center justify-between p-2 border-b dark:border-blue-800">
                 <button
-                    // TODO global state open sidebar
+                    onClick={handleMainOpen}
                     className="p-1 text-blue-400 transition-colors duration-200
                     rounded-md bg-blue-50 hover:text-blue-600 hover:bg-blue-100
                     dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark
@@ -39,8 +50,10 @@ export const Navbar = () => {
                 </a>
 
                 <button
-                    //TODO mobile submenu open
-                    className="p-1 text-blue-400 transition-colors duration-200 rounded-md bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark md:hidden focus:outline-none focus:ring"
+                    onClick={handleSubOpen}
+                    className="p-1 text-blue-400 transition-colors duration-200 rounded-md bg-blue-50
+                    hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700
+                    dark:bg-dark md:hidden focus:outline-none focus:ring"
                 >
                     <span className="sr-only">Open sub menu</span>
                     <span aria-hidden="true">
@@ -63,7 +76,9 @@ export const Navbar = () => {
 
                 <nav aria-label="Secondary" className="hidden space-x-2 md:flex md:items-center">
                     <button
-                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800"
+                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50
+                         hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700
+                         dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800"
                     >
                         <span className="sr-only">Open Notification panel</span>
                         <svg
@@ -83,7 +98,10 @@ export const Navbar = () => {
                         </svg>
                     </button>
                     <button
-                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800"
+                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full
+                        bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light
+                        dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100
+                        dark:focus:bg-blue-700 focus:ring-blue-800"
                     >
                         <span className="sr-only">Open search panel</span>
                         <svg
@@ -103,7 +121,10 @@ export const Navbar = () => {
                         </svg>
                     </button>
                     <button
-                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800"
+                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full
+                        bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light
+                        dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100
+                        dark:focus:bg-blue-700 focus:ring-blue-800"
                     >
                         <span className="sr-only">Open settings panel</span>
                         <svg
@@ -175,6 +196,142 @@ export const Navbar = () => {
                             </a>
                         </div>
                     </div>
+                </nav>
+
+                <nav
+                    className={`absolute flex items-center p-4 bg-white 
+                        rounded-md shadow-lg dark:bg-darker top-16 inset-x-4 md:hidden ${submenuOpen ? "" : "hidden"}`}
+                    aria-label="Secondary"
+                >
+                    <div className="space-x-2">
+                        <button
+                            className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50
+                         hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700
+                         dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800"
+                        >
+                            <span className="sr-only">Open Notification panel</span>
+                            <svg
+                                className="w-7 h-7"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                                />
+                            </svg>
+                        </button>
+                        <button
+                            className="p-2 text-blue-400 transition-colors duration-200 rounded-full
+                        bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light
+                        dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100
+                        dark:focus:bg-blue-700 focus:ring-blue-800"
+                        >
+                            <span className="sr-only">Open search panel</span>
+                            <svg
+                                className="w-7 h-7"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                />
+                            </svg>
+                        </button>
+                        <button
+                            className="p-2 text-blue-400 transition-colors duration-200 rounded-full
+                        bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light
+                        dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100
+                        dark:focus:bg-blue-700 focus:ring-blue-800"
+                        >
+                            <span className="sr-only">Open settings panel</span>
+                            <svg
+                                className="w-7 h-7"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                                />
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                            </svg>
+                        </button>
+
+                    </div>
+                    <div className="relative ml-auto">
+                        <button
+                            type="button"
+                            aria-haspopup="true"
+                            aria-expanded={profileOpen}
+                            onFocus={handleOpen}
+                            onBlur={handleOpen}
+                            className="transition-opacity duration-200 rounded-full dark:opacity-75 dark:hover:opacity-100 focus:outline-none focus:ring dark:focus:opacity-100"
+                        >
+                            <span className="sr-only">User menu</span>
+                            <img className="w-10 h-10 rounded-full"
+                                 src="https://cdn.discordapp.com/avatars/263349725099458566/b5b18f501c40f7ba6c8bc5f4085ab221.png?size=128"
+                                 alt="Ahmed Kamel"/>
+                        </button>
+                        <div
+                            className={`absolute right-0 w-48 py-1 bg-white
+                             rounded-md shadow-lg top-12 ring-1 ring-black 
+                             ring-opacity-5 dark:bg-dark focus:outline-none
+                             ${profileOpen ? "block" : "hidden"}`}
+                            tabIndex="-1"
+                            role="menu"
+                            aria-orientation="vertical"
+                            aria-label="User menu"
+                        >
+                            <a
+                                href="#"
+                                role="menuitem"
+                                className="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600"
+                            >
+                                Your Profile
+                            </a>
+                            <a
+                                href="#"
+                                role="menuitem"
+                                className="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600"
+                            >
+                                Settings
+                            </a>
+                            <a
+                                href="#"
+                                role="menuitem"
+                                className="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600"
+                            >
+                                Logout
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div className={`border-b md:hidden dark:border-primary-darker ${mainOpen ? "" : "hidden"}`}>
+                <nav aria-label="Main" className="px-2 py-4 space-y-2">
+                    {children}
                 </nav>
             </div>
         </header>
