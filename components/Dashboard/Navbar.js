0ce13dcd16e012b594b01380fe6446206776356d
zvshka <pushpurs@yandex.ru>
2021-12-13 11:05:14 +0300

    Next.js Version

--- components/Dashboard/Navbar.js
+++ components/Dashboard/Navbar.js
@@ -24,7 +24,7 @@ export const Navbar = ({children}) => {
             <div className="flex items-center justify-between p-2 border-b dark:border-blue-800">
                 <button
                     onClick={handleMainOpen}
-                    className="p-1 text-blue-400 transition-colors duration-200
+                    className="p-1 text-secondary transition-colors duration-200
                     rounded-md bg-blue-50 hover:text-blue-600 hover:bg-blue-100
                     dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark
                     md:hidden focus:outline-none focus:ring"
@@ -51,7 +51,7 @@ export const Navbar = ({children}) => {
 
                 <button
                     onClick={handleSubOpen}
-                    className="p-1 text-blue-400 transition-colors duration-200 rounded-md bg-blue-50
+                    className="p-1 text-secondary transition-colors duration-200 rounded-md bg-blue-50
                     hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700
                     dark:bg-dark md:hidden focus:outline-none focus:ring"
                 >
@@ -76,7 +76,7 @@ export const Navbar = ({children}) => {
 
                 <nav aria-label="Secondary" className="hidden space-x-2 md:flex md:items-center">
                     <button
-                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50
+                        className="p-2 text-secondary transition-colors duration-200 rounded-full bg-blue-50
                          hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700
                          dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800"
                     >
@@ -98,7 +98,7 @@ export const Navbar = ({children}) => {
                         </svg>
                     </button>
                     <button
-                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full
+                        className="p-2 text-secondary transition-colors duration-200 rounded-full
                         bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light
                         dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100
                         dark:focus:bg-blue-700 focus:ring-blue-800"
@@ -117,35 +117,6 @@ export const Navbar = ({children}) => {
                                 strokeLinejoin="round"
                                 strokeWidth="2"
                                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                            />
-                        </svg>
-                    </button>
-                    <button
-                        className="p-2 text-blue-400 transition-colors duration-200 rounded-full
-                        bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light
-                        dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100
-                        dark:focus:bg-blue-700 focus:ring-blue-800"
-                    >
-                        <span className="sr-only">Open settings panel</span>
-                        <svg
-                            className="w-7 h-7"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            aria-hidden="true"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                            />
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                             />
                         </svg>
                     </button>
@@ -178,21 +149,14 @@ export const Navbar = ({children}) => {
                                 role="menuitem"
                                 className="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600"
                             >
-                                Your Profile
+                                Профиль
                             </a>
                             <a
                                 href="#"
                                 role="menuitem"
                                 className="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600"
                             >
-                                Settings
-                            </a>
-                            <a
-                                href="#"
-                                role="menuitem"
-                                className="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600"
-                            >
-                                Logout
+                                Настройки
                             </a>
                         </div>
                     </div>
